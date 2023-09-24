c03ce3c550e28cf7a9680fa58b4c55c80713aeb8
dangdungvn <tungmj1605@gmail.com>
2023-09-24 19:30:35 +0700

    sua code

--- test1.py
+++ test1.py
@@ -1,10 +1,21 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import math
+def count_divisors(n):
+    count = 1
+    sqrt_n = int(math.sqrt(n))
+    
+    # Phân tích n thành các số nguyên tố mũ
+    for i in range(2, sqrt_n + 1):
+        power = 0
+        while n % i == 0:
+            n //= i
+            power += 1
+        count *= (power + 1)
+    
+    # Xử lý trường hợp n là một số nguyên tố lớn hơn sqrt(n)
+    if n > 1:
+        count *= 2
+    
+    return count
 
-
-x = [90,80,70,60,50,40,30,20,10]
-
-y = [53,98,183,37,122,14,124,65,67]
-plt.plot(x,y);
-plt.show();
+n = int(input())
+print(count_divisors(n))
