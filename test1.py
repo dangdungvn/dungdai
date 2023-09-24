7e26da81d3b8a87eff5005eba31cc439b7140eb4
dangdungvn <tungmj1605@gmail.com>
2023-09-24 22:04:53 +0700

    thu format

--- test1.py
+++ test1.py
@@ -1,8 +1,10 @@
 import math
+
+
 def count_divisors(n):
     count = 1
     sqrt_n = int(math.sqrt(n))
-    
+
     # Phân tích n thành các số nguyên tố mũ
     for i in range(2, sqrt_n + 1):
         power = 0
@@ -10,12 +12,13 @@ def count_divisors(n):
             n //= i
             power += 1
         count *= (power + 1)
-    
+
     # Xử lý trường hợp n là một số nguyên tố lớn hơn sqrt(n)
     if n > 1:
         count *= 2
-    
+
     return count
+
 
 n = int(input())
 print(count_divisors(n))
